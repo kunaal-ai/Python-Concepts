02c4ac85803eb16f6076550d5f2feaea0d06d8c8
Kunaal Thanik <kunaal@gmx.com>
2023-04-30 13:20:51 -0500

    linting and cleanup

---
+++ functions/arbitrary_arguments.py
@@ -0,0 +1,22 @@
+"""Explaining use of *args and **kwargs
+"""
+def arbitrary_arguments(prog_language, rating, *args):
+    """*args can take x no. of arguments
+    return as tuple
+    """
+    result = f"language = {prog_language} rating is {rating} and extra details {args}"
+    return result
+
+
+print(arbitrary_arguments('a', 3, 'a', 'r', 'g', 's'))
+
+def arbitrary_arguments_dic(prog_language, rating, **kwargs):
+    """**kwarg return as dictionary
+    """
+    kwargs['language'] = prog_language
+    kwargs['star_rating'] = rating
+    return kwargs
+
+
+response = arbitrary_arguments_dic('python', 3, level='master', years_of_exp=5)
+print(response)
