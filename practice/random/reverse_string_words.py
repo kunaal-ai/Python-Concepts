59251532233381dd4f6c9225aaed309f9f0ab4c7
Kunaal Thanik <kunaal@gmx.com>
2023-05-03 22:14:28 -0500

    Docstring

--- practice/random/reverse_string_words.py
+++ practice/random/reverse_string_words.py
@@ -5,6 +5,10 @@ Outout: "language my is Python"
 
 def reverse_string_words(user_string):
     """ return the string in reverse words order
+    Args:
+        user_String: (string)
+    Return:
+        return reveresed words
     """
     response = user_string.split(' ')[::-1]
     return " ".join(response)
