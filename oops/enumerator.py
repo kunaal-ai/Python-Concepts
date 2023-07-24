8973f02f55e94d20af234178f0832271a94294fa
KT <46636573+kunaal-ai@users.noreply.github.com>
2023-07-24 17:29:49 -0500

    Update enumerator.py
    removed end=""

--- oops/enumerator.py
+++ oops/enumerator.py
@@ -2,4 +2,4 @@ filename = '../Python-Concepts/test_document.txt'
 
 with open(filename) as file:
     for i, line in enumerate(file):
-        print(f"{i+1}:{line}", end="")
+        print(f"{i+1}:{line}")
